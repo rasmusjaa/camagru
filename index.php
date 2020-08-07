f8b9d17d1c020a3965c4b76cba6991c79182bccc
rasmusjaa <rasmus@lense.fi>
2020-08-07 17:43:45 +0300

    more basiscs

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php
 
-# lisaa favicon.ico roottiin
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="styles/style.css">
-	<link rel="icon" type="image/ico" href="favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="stylesheet" type="text/css" href="/styles/style.css"/>
+	<link rel="icon" type="image/ico" href="/favicon.ico"/>
+	<title>Camagru</title>
 </head>
 <body>
+	<div id="header">
+		<nav>
+			<div id="navi">
+				<ul>
+					<li>menu</li>
+					<li>menu</li>
+					<li>menu</li>
+					<li>menu</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+	<div id="main">
 
+	</div>
+	<div id="footer">
+
+	</div>
 </body>
 </html>
