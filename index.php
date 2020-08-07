8597e621ac8734e61ec80e13a22ff64ef70a538a
rasmusjaa <rasmus@lense.fi>
2020-08-07 17:02:57 +0300

    basic files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+# lisaa favicon.ico roottiin
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<link rel="icon" type="image/ico" href="favicon.ico">
+</head>
+<body>
+
+</body>
+</html>
